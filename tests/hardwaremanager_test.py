8c00c2f49276ceff8d948b009f4428b1f15c16a2
Joris Herbots <joris.herbots@student.uhasselt.be>
2018-05-25 16:29:36 +0200

    Event documentation & testfile bugfix

--- tests/hardwaremanager_test.py
+++ tests/hardwaremanager_test.py
@@ -3,12 +3,12 @@ from engine.hardware import led,three_button,vibration_motor
 
 def test_harware_parser_dict_name():
     hwm = hardwaremanager.HardwareManager()
-    assert led._name_id in hwm.hardware_configuration_parsers
-    assert three_button._name_id in hwm.hardware_configuration_parsers
-    assert vibration_motor._name_id in hwm.hardware_configuration_parsers
+    assert str(led._name_id) in hwm.hardware_configuration_parsers
+    assert str(three_button._name_id) in hwm.hardware_configuration_parsers
+    assert str(vibration_motor._name_id) in hwm.hardware_configuration_parsers
 
 def test_harware_parser_dict_method_pointer():
     hwm = hardwaremanager.HardwareManager()
-    assert hwm.hardware_configuration_parsers[led._name_id] == led.parse_config
-    assert hwm.hardware_configuration_parsers[three_button._name_id] == three_button.parse_config
-    assert hwm.hardware_configuration_parsers[vibration_motor._name_id] == vibration_motor.parse_config
+    assert hwm.hardware_configuration_parsers[str(led._name_id)] == led.parse_config
+    assert hwm.hardware_configuration_parsers[str(three_button._name_id)] == three_button.parse_config
+    assert hwm.hardware_configuration_parsers[str(vibration_motor._name_id)] == vibration_motor.parse_config
